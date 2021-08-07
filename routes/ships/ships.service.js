925d7da599c0e38cdd1469891598bf8833b511e6
featherwalk <vacantorange@gmail.com>
2021-08-07 15:13:23 -0500

    added ships service - list by server, player, starter, class

---
+++ routes/ships/ships.service.js
@@ -0,0 +1,21 @@
+const knex = require("../../db/connection");
+
+function listByServer(server_name) {
+  return knex("ships")
+    .select("ship_name", "ship_id")
+    .where("server_name", server_name);
+}
+
+function listByPlayer(player_discord_id) {
+  return knex("ships")
+    .select("ship_name", "ship_id")
+    .where("player_discord_id", player_discord_id);
+}
+
+function listStarterShips() {
+  return knex("ship_types").select("*").where("ship_class", "starter");
+}
+
+function listTypes(ship_class) {
+  return knex("ship_types").select("*").where("ship_class", ship_class);
+}
