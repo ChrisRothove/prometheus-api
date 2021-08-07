1aab26fa53cf8730242f91657760248361e180fe
featherwalk <vacantorange@gmail.com>
2021-08-07 15:04:38 -0500

    list, update, create added to players.service

--- routes/players/players.service.js
+++ routes/players/players.service.js
@@ -3,3 +3,20 @@ const knex = require("../../db/connection");
 function list() {
   return knex("players").select("*");
 }
+
+function update(player_discord_id, input) {
+  return knex("players")
+    .where("player_discord_id", player_discord_id)
+    .update(input, "*")
+    .then((result) => result[0]);
+}
+
+function create(input) {
+  return knex("players").insert(input, "*");
+}
+
+module.exports = {
+  list,
+  update,
+  create,
+};
